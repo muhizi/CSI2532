918e70b2010b248dc7e49a6aa3260d81e733cf55
Martin Hanzel <arthanzel@gmail.com>
2016-03-15 17:00:24 -0400

    Initial

---
+++ sql/createData.sql
@@ -0,0 +1,10 @@
+SET search_path = Pharmacy;
+
+INSERT INTO Doctor ('Han', 'Solo', 'Cockpit, Millenium Falcon', 'Han shot first', 'Womanizing'); -- 1
+INSERT INTO Doctor ('Chewbacca', 'Wookie', 'Passenger seat, Millenium Falcon', 'Rrrrrrr-ghghghghgh!', 'Veterinary Medicine'); -- 2
+INSERT INTO Doctor ('Darth', 'Vader', 'Room 616, 31st Floor, Death Star', '616-616-6666', 'Transhumanism'); -- 3
+
+INSERT INTO Patient ('Luke', 'Skywalker', DATE'2981-01-27', 'Moisture Farm, Tatooine', '616-616-6666', 'M', '123456789'); -- 1
+INSERT INTO Patient ('Anakin', 'Skywalker', DATE'2958-08-12', 'Jedi Temple, Coruscant', '616-616-6666', 'M', '123456789'); -- 2
+INSERT INTO Patient ('Leia', 'Organa', DATE'2981-01-27', 'Citadel, Naboo', '616-616-6666', 'F', '123456789'); -- 3
+
---
+++ sql/createSchema.sql
@@ -0,0 +1,84 @@
+DROP SCHEMA IF EXISTS Pharmacy CASCADE;
+CREATE SCHEMA Pharmacy;
+
+SET search_path = Pharmacy;
+
+CREATE TABLE Doctor (
+    firstName VARCHAR(255) NOT NULL,
+    lastName VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    tel VARCHAR(255) NOT NULL,
+    specialty VARCHAR(255),
+    id SERIAL PRIMARY KEY
+);
+
+CREATE TABLE Patient (
+    firstName VARCHAR(255) NOT NULL,
+    lastName VARCHAR(255) NOT NULL,
+    birthDate DATE NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    tel VARCHAR(255) NOT NULL,
+    sex CHAR(1) NOT NULL,
+    CONSTRAINT checkSex CHECK (sex = 'M' OR sex = 'F'),
+    ssn VARCHAR(255) UNIQUE NOT NULL,
+    id SERIAL PRIMARY KEY
+);
+
+CREATE TABLE Appointment (
+    date TIMESTAMP NOT NULL,
+    endDate TIMESTAMP NOT NULL,
+    remarks TEXT,
+    patient INTEGER REFERENCES Patient NOT NULL,
+    doctor INTEGER REFERENCES Doctor NOT NULL,
+    id SERIAL PRIMARY KEY
+);
+
+CREATE TABLE Drug (
+    name VARCHAR(255) NOT NULL,
+    price NUMERIC(11, 2) NOT NULL,
+    substance VARCHAR(255) NOT NULL,
+    generic BOOLEAN NOT NULL,
+    id SERIAL PRIMARY KEY
+);
+
+CREATE TABLE DrugScript (
+    drug INTEGER REFERENCES Drug NOT NULL,
+    doctor INTEGER REFERENCES Doctor NOT NULL,
+    patient INTEGER REFERENCES Patient NOT NULL,
+    date DATE NOT NULL,
+    dateFulfilled DATE,
+    endValidDate DATE NOT NULL,
+    id SERIAL PRIMARY KEY
+);
+
+CREATE TABLE ProcScript (
+    procName VARCHAR(255) NOT NULL,
+    doctor INTEGER REFERENCES Doctor NOT NULL,
+    patient INTEGER REFERENCES Patient NOT NULL,
+    date DATE NOT NULL,
+    dateFulfilled DATE,
+    id SERIAL PRIMARY KEY
+);
+
+CREATE TABLE DrugConflict (
+    substance1 VARCHAR(255) NOT NULL,
+    substance2 VARCHAR(255) NOT NULL,
+    id SERIAL PRIMARY KEY
+);
+
+CREATE TABLE Pathology (
+    name VARCHAR(255) NOT NULL,
+    id SERIAL PRIMARY KEY
+);
+
+CREATE TABLE DrugPathologyConflict (
+    substance VARCHAR(255) NOT NULL,
+    pathology INTEGER REFERENCES Pathology NOT NULL,
+    id SERIAL PRIMARY KEY
+);
+
+CREATE TABLE PatientPathology (
+    patient INTEGER REFERENCES Patient NOT NULL,
+    pathology INTEGER REFERENCES Pathology NOT NULL,
+    id SERIAL PRIMARY KEY
+);
